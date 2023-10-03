3108c494c3189f24e0e6355c81402b86d1004836
Quade Martin <probablyquade@gmail.com>
2023-10-02 21:12:30 -0500

    Updated vae_compression and logbook
    Changed vae_compression to include proper imports and took a different approach to implementing the model as the previous one was not working. It does not fully work yet but I hope to have the new error fixed when possible. Updated logbook to reflect this as well as include some needed links.

--- logbook.md
+++ logbook.md
@@ -9,3 +9,11 @@ Installed essential libraries Pytorch, Diffusers, and Transformers to python env
 Wrote code that initializes a prompt and generates an image using it.
 
 Changed README to include more information such as the project's ultimate final goal.
+
+## 10-2-2023
+
+[link to test code](https://towardsdatascience.com/stable-diffusion-using-hugging-face-501d8dbdd8)
+
+[from_pretrained help](https://huggingface.co/docs/diffusers/using-diffusers/loading)
+
+Worked on code to utilize model as the previous code was not working despite no major errors occuring. (The model was not found)
--- vae_compression.py
+++ vae_compression.py
@@ -1,8 +1,22 @@
 import torch, logging
 from transformers import CLIPTextModel, CLIPTokenizer
-from diffusers import StableDiffusionPipeline 
+from diffusers import DiffusionPipeline, StableDiffusionPipeline, StableDiffusionImg2ImgPipeline
+import huggingface_hub
 
-pipe = StableDiffusionPipeline.from_pretrained('runwayml/stable-diffusion-v1-5').to('cuda')
+# print(torch.cuda.is_available())
+
+# ## disable warnings
+# logging.disable(logging.WARNING)  
+
+# ## Import the CLIP artifacts 
+# from transformers import CLIPTextModel, CLIPTokenizer
+
+# ## Initiating tokenizer and encoder.
+# tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14", torch_dtype=torch.float16)
+# text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14", torch_dtype=torch.float16).to("cuda")
+
+repo_id = "./v1-5-pruned-emaonly/"
+pipe = DiffusionPipeline.from_pretrained(repo_id, use_safetensors=True)
 
 # Initialize a prompt
 prompt = "a cat wearing glasses"
